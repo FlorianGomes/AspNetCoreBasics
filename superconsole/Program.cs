72957c688a13b4b800f16f1a00272f1d52fb26b9
Florian Gomes <gomesflorian2015@gmail.com>
2017-04-06 15:40:05 +0530

    Added the demo console
    Console app that shows us how AspNetCore works and can be used across
    platforms

---
+++ superconsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace superconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            
+            Console.WriteLine($"Hello {name}");
+            
+            string ex = Console.ReadLine();
+            
+            if(ex == "exit")
+				;
+            
+            
+        }
+    }
+}
